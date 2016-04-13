84f9f0a5fb0a30764f7fe61aafb95eedee7b83d4
Andres Yepes <yepes07@gmail.com>
2016-04-13 18:54:32 -0500

    add express and socket.io modules, and create a chat room with debora

--- detector.js
+++ detector.js
@@ -1,10 +1,36 @@
+var readline   = require('line-reader');
 var Classifier = require("classifier");
 var detector   = new Classifier.Bayesian();
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
-detector.train("Combien des élèves y a-t-il dans votre collège?", "french");
-detector.train("Voulez-vous peser ce colis, s'il vous plaît.", "french");
-detector.train("Ciao, non ti va di andare al cinema?", "italian");
-detector.train("Mi sai dire quando apre il negozio?", "italian");
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
 
-console.log(detector.classify("Non so quando andare il cinema"));
-console.log(detector.classify("me"));
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    var res = {state: detector.classify(msg), msg: msg}
+    console.log(res.state + ': ' + res.msg);
+    io.emit('chat response', res);
+  });
+});
+
+http.listen(3000, function () {
+  readline.eachLine('./data/angry.txt', function(line, last) {
+      detector.train(line, "angry");
+      console.log(line);
+      if(last){
+        readline.eachLine('./data/happy.txt', function(line, last) {
+            detector.train(line, "happy");
+            console.log(line);
+            if(last){
+              console.log('Devora Trained and listening on port 3000!');
+            }
+        });
+      }
+  });
+
+});
